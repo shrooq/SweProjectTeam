58e6a7291e75a8a9e918208c3b0dbac09bf74df2
Aboud <abdo_firefox_10@hotmail.com>
2015-03-02 08:27:17 -0800

    add

---
+++ src/com/fci/swe/models/UserEntity.java
@@ -0,0 +1,177 @@
+package com.FCI.SWE.Models;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+
+/**
+ * <h1>User Entity class</h1>
+ * <p>
+ * This class will act as a model for user, it will holds user data
+ * </p>
+ *
+ * @author Mohamed Samir
+ * @version 1.0
+ * @since 2014-02-12
+ */
+public class UserEntity {
+	private String name;
+	private String email;
+	private String password;
+	public ArrayList<String> friends;
+	public ArrayList<String> requests;
+	public static UserEntity loggedUser;
+	/**
+	 * Constructor accepts user data
+	 * 
+	 * @param name
+	 *            user name
+	 * @param email
+	 *            user email
+	 * @param password
+	 *            user provided password
+	 */
+	public UserEntity(String name, String email, String password) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		friends=new ArrayList<String>();
+		requests=new ArrayList<String>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPass() {
+		return password;
+	}
+
+	/**
+	 * 
+	 * This static method will form UserEntity class using json format contains
+	 * user data
+	 * 
+	 * @param json
+	 *            String in json format contains user data
+	 * @return Constructed user entity
+	 */
+	public static UserEntity getUser(String email) {
+
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		Query gaeQuery = new Query("users");
+		PreparedQuery pq = datastore.prepare(gaeQuery);
+		for (Entity entity : pq.asIterable()) {
+			System.out.println(entity.getProperty("name").toString());
+			if (entity.getProperty("email").toString().equals(email)){
+				UserEntity returnedUser = new UserEntity(entity.getProperty(
+						"name").toString(), entity.getProperty("email")
+						.toString(), entity.getProperty("password").toString());
+				return returnedUser;
+			}
+		}
+
+		return null;
+
+	}
+
+	/**
+	 * 
+	 * This static method will form UserEntity class using user name and
+	 * password This method will serach for user in datastore
+	 * 
+	 * @param name
+	 *            user name
+	 * @param pass
+	 *            user password
+	 * @return Constructed user entity
+	 */
+
+	public static UserEntity getUser(String name, String pass) {
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+
+		Query gaeQuery = new Query("users");
+		PreparedQuery pq = datastore.prepare(gaeQuery);
+		for (Entity entity : pq.asIterable()) {
+			System.out.println(entity.getProperty("name").toString());
+			if (entity.getProperty("name").toString().equals(name)
+					&& entity.getProperty("password").toString().equals(pass)) {
+				UserEntity returnedUser = new UserEntity(entity.getProperty(
+						"name").toString(), entity.getProperty("email")
+						.toString(), entity.getProperty("password").toString());
+				loggedUser=returnedUser;
+				return returnedUser;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * This method will be used to save user object in datastore
+	 * 
+	 * @return boolean if user is saved correctly or not
+	 */
+	public Boolean saveUser() {
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		Query gaeQuery = new Query("users");
+		PreparedQuery pq = datastore.prepare(gaeQuery);
+		List<Entity> list = pq.asList(FetchOptions.Builder.withDefaults());
+
+		Entity employee = new Entity("users", list.size() + 1);
+
+		employee.setProperty("name", this.name);
+		employee.setProperty("email", this.email);
+		employee.setProperty("password", this.password);
+		datastore.put(employee);
+
+		return true;
+	}
+	public Boolean saveFriends(UserEntity user) {
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+
+		Query gaeQuery = new Query("users");
+		PreparedQuery pq = datastore.prepare(gaeQuery);
+		for (Entity entity : pq.asIterable()) {
+			if (entity.getProperty("email").toString().equals(user.getEmail())) {
+				entity.setProperty("friends",user.friends);
+				return true;
+			}
+		}
+		return false;
+	}
+	public Boolean saveRequests(UserEntity user) {
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+
+		Query gaeQuery = new Query("users");
+		PreparedQuery pq = datastore.prepare(gaeQuery);
+		for (Entity entity : pq.asIterable()) {
+			if (entity.getProperty("email").toString().equals(user.getEmail())) {
+				entity.setProperty("requests",user.requests);
+				return true;
+			}
+		}
+		return false;
+	}
+}
